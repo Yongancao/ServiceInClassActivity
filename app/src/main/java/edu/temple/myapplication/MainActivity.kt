9e6d42e8906e493043b46620c4f2ea1c2803d6c7
Yongancao <tup90476@temple.edu>
2026-04-02 12:08:36 -0400

    timer running

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,20 +1,65 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
+import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timerBinder : TimerService.TimerBinder
+    var isConnected = false
+
+    val timerHandler = android.os.Handler(Looper.getMainLooper()) {
+        findViewById<TextView>(R.id.textView).text = it.what.toString()
+        true
+    }
+
+    val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+            timerBinder.setHandler(timerHandler)
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isConnected = false
+        }
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val timerTextView = findViewById<TextView>(R.id.textView)
+
+
+        bindService(Intent(this, TimerService::class.java), serviceConnection, BIND_AUTO_CREATE)
+
         findViewById<Button>(R.id.startButton).setOnClickListener {
+            if(timerBinder.isRunning) {
+                timerBinder.pause()
+            } else if (timerBinder.paused){
+                timerBinder.pause()
+            }
+            if (isConnected) {
+                timerBinder.start(10)
+            }
 
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
+            if (isConnected) {
+                timerBinder.stop()
+            }
 
         }
     }
